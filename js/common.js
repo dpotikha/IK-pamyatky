8ec28783607e42f95e73d6571bb69500f4917f8e
Yevgen <godun.yevgen@gmail.com>
2013-02-25 17:09:26 +0200

    upd

--- js/common.js
+++ js/common.js
@@ -1,3 +1,17 @@
 $(document).ready(function() {
 
+// ---------------- Input drop-list -------------------------- //
+    // show/hide
+    $(".js-input").focusin(function(){
+        $(this).next().next().fadeIn("fast");
+    });
+    $(".js-input").focusout(function(){
+        $(this).next().next().fadeOut("fast");
+    });
+    // get text
+    $(".drop-list li").click(function(){
+        var text = $(this).children("span").text();
+        $(this).parent().prev().prev().val(text);
+    });
+
 });
